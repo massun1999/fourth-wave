6f18b982799551a6e04e79572c3c57fd89bc1b67
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-15 18:42:24 +0900

    テストコードの準備

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録ができる時' do
+      it "全ての項目が正しく入力されている時" do
+        
+      end
+    end
+  end
+end
