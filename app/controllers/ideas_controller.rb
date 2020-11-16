fe9f111bcaf6970ed38eb626b670c40b8db30afe
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-16 15:35:29 +0900

    createアクションを定義

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -9,4 +9,19 @@ class IdeasController < ApplicationController
     @idea = Idea.new
   end
 
+  def create
+    @idea = Idea.new(idea_params)
+    if @idea.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :text, :copy, :category_id, :format_id, :user_id).merge(user_id: current_user.id)
+  end
+
 end
