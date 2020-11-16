779810e423009965d20e4bcb0a5e53b4ac0f03e6
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-16 17:14:30 +0900

    バリデーションの編集

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,12 +5,14 @@ class Idea < ApplicationRecord
   belongs_to_active_hash :format
 
   belongs_to :user
+  has_one_attached :image
 
 
   with_options presence: true do
-    validates :title
-    validates :copy
+    validates :title, length: { maximum: 40}
     validates :text
+    validates :price, numericality: {:less_than => 1000001}, numericality: {:greater_than => 1}
+    validates :image
   end
 
   with_options numericality: { other_than: 1 } do
