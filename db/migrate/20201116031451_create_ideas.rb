deb44ffc450430b530452e6de0df197067db32c6
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-16 17:15:20 +0900

    テーブル編集

--- db/migrate/20201116031451_create_ideas.rb
+++ db/migrate/20201116031451_create_ideas.rb
@@ -2,10 +2,10 @@ class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
       t.string      :title, null:false
-      t.string       :copy, null:false
       t.text        :text, null:false
       t.integer     :category_id, null:false
       t.integer     :format_id, null:false
+      t.integer     :price, null:false
       t.references  :user , null:false, foreign_key:true
       t.timestamps
     end
