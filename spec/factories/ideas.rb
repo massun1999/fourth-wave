327a8651ecd38d51634ed02349c14308ab541a6c
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-16 17:14:09 +0900

    activestrage導入

--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :idea do
+    title          {Faker::name}
+    text           {Faker::Lorem.sentence}
+    category_id    {2}
+    format_id      {2}
+    price          {2000}
+
+    association :user
+
     
   end
 end
