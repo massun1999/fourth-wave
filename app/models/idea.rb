32d01c5f60a5414801af308c4797f742c62b400e
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-16 15:07:11 +0900

    バリデーションを設定

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,19 @@
 class Idea < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :format
+
+
+  with_options presence: true do
+    validates :title
+    validates :copy
+    validates :text
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :format_id
+  end
+
 end
