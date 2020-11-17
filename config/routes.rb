27d46ea227cd11cd50058774f4d687dedb95cd95
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-17 11:02:15 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :ideas, only: [:index, :new, :create, :show]
+  resources  :orders, only: [:index, :create]
   root to: "ideas#index"
 end
