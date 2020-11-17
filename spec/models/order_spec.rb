db686cb91020de2db0cb7c71623730484793f706
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-17 16:57:36 +0900

    モデル単体テストコード

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  it "tokenがあれば登録できる" do
+    expect(@order).to be_valid
+  end
+
+  it "tokenが空では登録できない" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
+  end
+
+  it "userが紐づいていないと登録できない" do
+    @order.user = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("User must exist")
+  end
+  it "ideaが紐づいていないと登録できない" do
+    @order.idea = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Idea must exist")
+  end
 end
