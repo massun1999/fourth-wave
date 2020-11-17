add722cb76026bb9b68e87af1d561613d4b36452
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-17 19:26:02 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :ideas, only: [:index, :new, :create, :show] do
     resources  :orders, only: [:index, :create]
   end
+  resources :users, only: [:show, :edit, :update]
   root to: "ideas#index"
 end
