02748a9b0b3c16981237d5a6d2d8a94a94ad59bc
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-19 12:47:42 +0900

    バリデーションの変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
 
   with_options presence: true do
     validates :token
+    validates :price, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 1000000 }
   end
 
   belongs_to :user
