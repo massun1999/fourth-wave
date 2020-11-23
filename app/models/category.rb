d5f4ce87a8dd24e4c215fb7017a78e508b058c72
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-23 11:08:58 +0900

    フロント編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,10 @@ class Category < ActiveHash::Base
 
   self.data = [
     { id: 1, name: '__' },
-    { id: 2, name: 'テクノロジー'},
-    { id: 3, name: '生活雑貨'},
-    { id: 4, name: '飲食店'},
-    { id: 5, name: 'イベント'},
+    { id: 2, name: 'PourOver'},
+    { id: 3, name: 'Espresso'},
+    { id: 4, name: 'AeroPress'},
+    { id: 5, name: 'FrenchPress'},
     { id: 6, name: 'その他'}
   ]
   include ActiveHash::Associations
--- app/models/format.rb
+++ app/models/format.rb
@@ -2,8 +2,8 @@ class Format < ActiveHash::Base
   
   self.data = [
     { id: 1, name: '__' },
-    { id: 2, name: '売却希望' },
-    { id: 3, name: '共同制作' },
+    { id: 2, name: 'Recipe' },
+    { id: 3, name: 'Method' },
     { id: 4, name: 'コメントのみ希望' }
   ]
 
