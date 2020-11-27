4a6d999868faabced252010447f5d32465e093d9
massun1999 <73097630+massun1999@users.noreply.github.com>
2020-11-27 18:59:56 +0900

    aws s3の設定

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -6,7 +6,7 @@ class Idea < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_many :comments, dependent: :destroy
 
 
